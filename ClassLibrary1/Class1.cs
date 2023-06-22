f6afec3a986688129c11032f70b1eb602ebe44d6
Akagami no Shanks <89997083+MustafaAlibekov@users.noreply.github.com>
2023-06-22 15:22:20 +0300

    ekz

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace Arifmetica
+{
+    public class Calculator
+    {
+        public static double MinAVG(string[] marks)
+        {
+            double sum = 0;
+            foreach (string mark in marks)
+            {
+                if (double.TryParse(mark, out double value))
+                {
+                    sum += value;
+                }
+            }
+
+            double avg = sum / marks.Length;
+            double roundedAvg = Math.Floor(avg * 10) / 10;
+
+            return roundedAvg;
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Arifmetica;
+
+namespace testArifmetica
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestMethod]
+        public void MinAVG_ReturnsCorrectRoundedAverage()
+        {
+            string[] marks = { "2", "4", "3", "4", "5" };
+            double expected = 3.6;
+            double actual = Calculator.MinAVG(marks);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void MinAVG_ReturnsCorrectRoundedAveragea()
+        {
+            string[] marks = { "2", "2", "3", "4", "4" };
+            double expected = 3;
+            double actual = Calculator.MinAVG(marks);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
